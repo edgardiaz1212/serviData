1832a1fda67bafe21fe8a9c8385e86bb16826949
Edgar Diaz <ed35376@gmail.com>
2025-02-03 12:14:44 -0400

    Update InputService.jsx

--- src/front/js/component/InputService.jsx
+++ src/front/js/component/InputService.jsx
@@ -1,8 +1,12 @@
 import React, { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Context } from '../store/appContext'; // Import Context to access actions
 import DatosServicio from '../component/DatosServicio.jsx'; // Import DatosServicio component
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function InputService({ clientData }) {
+  const navigate = useNavigate(); // Initialize navigate for navigation
   const { actions } = useContext(Context); // Access actions from context
   const [serviceData, setServiceData] = useState({
     dominio: '',
@@ -41,26 +45,40 @@ console.log('clientData:', clientData);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      await actions.addServiceData({ ...serviceData, cliente_id: clientData.id }); // Send data to backend
-      console.log('Service data submitted:', serviceData);
-    } catch (error) {
-      console.error('Error submitting service data:', error);
+    if (e.target.checkValidity() === false) {
+      e.stopPropagation();
+      toast.error('Por favor, complete todos los campos obligatorios.');
+    } else {
+      try {
+        await actions.addServiceData({ ...serviceData, cliente_id: clientData.id }); // Send data to backend
+        toast.success('Datos del servicio enviados correctamente');
+        console.log('Service data submitted:', serviceData);
+        setTimeout(() => {
+          navigate("/data-registro"); // Redirect to Manual Data Entry if client not found
+        }, 1500)
+      } catch (error) {
+        toast.error('Error al enviar los datos del servicio');
+        console.error('Error submitting service data:', error);
+      }
     }
+    e.target.classList.add('was-validated');
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h3>Cliente : {clientData.razon_social}</h3>
-      <DatosServicio
-        clientData={clientData}
-        serviceData={serviceData}
-        handleChange={handleChange}
-      />
-      <button className="btn btn-success mt-2" type="submit">
-        Guardar
-      </button>
-    </form>
+    <>
+      <form className="row g-3 needs-validation" onSubmit={handleSubmit} noValidate>
+        <h3>Cliente : {clientData.razon_social}</h3>
+        <DatosServicio
+          clientData={clientData}
+          serviceData={serviceData}
+          handleChange={handleChange}
+        />
+        <button className="btn btn-success mt-2" type="submit">
+          Guardar
+        </button>
+      </form>
+      <ToastContainer />
+    </>
   );
 }
 
