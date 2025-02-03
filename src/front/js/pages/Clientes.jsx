8f3a8614f84e5aa7dc2626e4aacb51338d776dc3
Edgar Diaz <ed35376@gmail.com>
2025-02-03 11:34:41 -0400

    Update Clientes.jsx

--- src/front/js/pages/Clientes.jsx
+++ src/front/js/pages/Clientes.jsx
@@ -1,30 +1,63 @@
-import React, { useState } from 'react';
-import TotalCLient from '../component/TotalCLient.jsx';
-import { Search } from 'lucide-react'
+import React, { useState } from "react";
+import TotalCLient from "../component/TotalCLient.jsx";
+import { Search } from "lucide-react";
 
 function Clientes() {
-  const [clientType, setClientType] = useState('Privada');
+  const [clientType, setClientType] = useState("Privada");
 
   const handleClientTypeChange = (event) => {
-    setClientType(event.target.id === 'btnradio1' ? 'Privada' : 'Publica');
+    setClientType(event.target.id === "btnradio1" ? "Privada" : "Publica");
   };
 
   return (
     <>
-      <div className='container'>
+      <div className="container">
         <h1>Clientes</h1>
-        <div className="btn-group" role="group" aria-label="Basic radio toggle button group">
-          <input type="radio" className="btn-check" name="btnradio" id="btnradio1" autoComplete="off" checked={clientType === 'Privada'} onChange={handleClientTypeChange} />
-          <label className="btn btn-outline-primary" htmlFor="btnradio1">Privada</label>
+        <div className="d-flex justify-content-between align-items-center mb-3">
+        <div
+          className="btn-group"
+          role="group"
+          aria-label="Basic radio toggle button group"
+        >
+          <input
+            type="radio"
+            className="btn-check"
+            name="btnradio"
+            id="btnradio1"
+            autoComplete="off"
+            checked={clientType === "Privada"}
+            onChange={handleClientTypeChange}
+          />
+          <label className="btn btn-outline-primary" htmlFor="btnradio1">
+            Privada
+          </label>
 
-          <input type="radio" className="btn-check" name="btnradio" id="btnradio2" autoComplete="off" checked={clientType === 'Publica'} onChange={handleClientTypeChange} />
-          <label className="btn btn-outline-primary" htmlFor="btnradio2">Publica</label>
-        
-          <div className="input-group">
-    <div className="input-group-text" id="btnGroupAddon"><Search /></div>
-    
-    <input type="text" className="form-control" placeholder="Busqueda" aria-label="Input group example" aria-describedby="btnGroupAddon"/>
-  </div>
+          <input
+            type="radio"
+            className="btn-check"
+            name="btnradio"
+            id="btnradio2"
+            autoComplete="off"
+            checked={clientType === "Publica"}
+            onChange={handleClientTypeChange}
+          />
+          <label className="btn btn-outline-primary" htmlFor="btnradio2">
+            Publica
+          </label>
+</div>
+          <div className="input-group"  style={{ maxWidth: '300px' }}>
+            <div className="input-group-text" id="btnGroupAddon">
+              <Search />
+            </div>
+
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Busqueda"
+              aria-label="Input group example"
+              aria-describedby="btnGroupAddon"
+            />
+          </div>
         </div>
         <TotalCLient clientType={clientType} />
       </div>
