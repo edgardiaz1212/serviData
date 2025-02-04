9b1a235b3f92ba15274f2a60d5787da0d3eda0b7
Edgar Diaz <ed35376@gmail.com>
2025-02-04 08:13:57 -0400

    cambios dashboard

--- src/front/js/pages/dashboard.jsx
+++ src/front/js/pages/dashboard.jsx
@@ -2,14 +2,23 @@ import React, { useEffect, useState, useContext } from 'react';
 
 
 import { Context } from "../store/appContext";
+import TipoServicios from '../component/TipoServicios.jsx';
 
 const Dashboard = () => {
   const { actions } = useContext(Context)
   
 
   return (
-   <>
- 
+  <>
+    <div className="text-bg-light p-3">Bienvenido a la plataforma de servicios!</div>
+    <div className='container'>
+
+
+
+
+
+      <TipoServicios/>
+    </div>
    </>
   );
 };
