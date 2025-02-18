7ae7e3bcec60211a5f327d1d838b5a337a02888b
Edgar Diaz <ed35376@gmail.com>
2025-02-18 13:15:59 -0400

    Update Reportes.jsx
    datos base

--- src/front/js/pages/Reportes.jsx
+++ src/front/js/pages/Reportes.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function Reportes() {
   return (
-    <div>Reportes</div>
+    <>
+    <h3>Clientes</h3>
+    <p>Clientes activos</p>
+    <p>Clientes Publica</p>
+    <p>Clientes Privada</p>
+
+    <h3>Servicios</h3>
+    <p>Servicios activos</p>
+    <p>Servicios Publica</p>
+    <p>Servicios Privada</p>
+    <p>Servicios retirados por mes</p>
+    <p> Servicios aprovisionados por mes</p>
+    </>
   )
 }
 
