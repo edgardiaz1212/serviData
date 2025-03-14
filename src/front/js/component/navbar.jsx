b938eb746fb215d59fc424df3c0419a33d9eb75a
Edgar Diaz <ed35376@gmail.com>
2025-03-14 08:06:56 -0400

    navbar hamburguer

--- src/front/js/component/navbar.jsx
+++ src/front/js/component/navbar.jsx
@@ -1,82 +1,92 @@
 import React, { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
-import logo from "../../img/CDHLogo.jpg"
-import {Cog} from 'lucide-react'
+import logo from "../../img/CDHLogo.jpg";
+import { Cog } from 'lucide-react';
 
 const Navbar = () => {
   const { store, actions } = useContext(Context);
   const navigate = useNavigate();
   const { isAuthenticated, user } = store;
-
+  
   const handleLogout = async () => {
     await actions.LogOut();
     navigate("/"); // Redirect user to the login page after logging out
   };
-
+  
   return (
-    <nav className="navbar navbar-expand-lg" style={{ backgroundColor: "#001f3f" }}>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <Link className="navbar-brand text-white" to="/">
+        <Link className="navbar-brand d-flex align-items-center" to="/">
+          <img src={logo} alt="Logo" width="30" height="30" className="me-2" />
           ServiData
-          <img  className="logo-icon " src={logo} alt="Logo" />
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
+        
+        {/* Hamburger menu toggle button */}
+        <button 
+          className="navbar-toggler" 
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbarNavDropdown" 
+          aria-controls="navbarNavDropdown" 
+          aria-expanded="false" 
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-          <ul className="navbar-nav">
+        
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             {isAuthenticated && (
               <>
-              <li className="nav-item">
-                  <Link className="nav-link text-white" to="/resumen">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/resumen">
                     Resumen
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link text-white" to="/clientes">
+                  <Link className="nav-link" to="/clientes">
                     Clientes
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link text-white" to="/data-registro">
+                  <Link className="nav-link" to="/registro">
                     Registro
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link text-white" to="/aprovisionados">
+                  <Link className="nav-link" to="/aprovisionados">
                     Aprovisionados
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link text-white" to="/reportes">
+                  <Link className="nav-link" to="/reportes">
                     Reportes
                   </Link>
-                </li>
-                <li className="nav-item">
-                  <span className="nav-link text-white">Hola, {user ? user.username : 'Usuario'}!</span>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link text-white" to="/user-register">
-                  <Cog />
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <button className="btn btn-link nav-link text-white" onClick={handleLogout}>
-                    Logout
-                  </button>
                 </li>
               </>
             )}
           </ul>
+          
+          {isAuthenticated && (
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <span className="nav-link">
+                  Hola, {user ? user.username : 'Usuario'}!
+                </span>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/settings">
+                  <Cog size={18} />
+                </Link>
+              </li>
+              <li className="nav-item">
+                <button className="nav-link btn btn-link" onClick={handleLogout}>
+                  Salir
+                </button>
+              </li>
+            </ul>
+          )}
         </div>
       </div>
     </nav>
