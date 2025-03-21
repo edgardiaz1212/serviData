55fcade171592fce5cdf673e004800bbadec43ae
Edgar Diaz <ed35376@gmail.com>
2025-03-21 09:15:04 -0400

    desing

--- src/front/js/component/footer.jsx
+++ src/front/js/component/footer.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import logo from '../../img/mad_data.png';
 
 const Footer = () => {
     return (
         <footer>
-            <p>© 2025 ServiData •  </p>
-           
+           <div className="d-flex justify-content-center align-items-center">
+            <p className='flex-grow-1'>© 2025 ServiData •  </p>
+            <img className="logo-icon" src={logo} alt="icon" width="70" />
+        </div>
         </footer>
     );
 };
-
 export default Footer;
