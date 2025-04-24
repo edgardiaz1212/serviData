b5a95b4b6010e9501446b2b016c1a2a2ef1f5d26
Edgar Diaz <ed35376@gmail.com>
2025-04-24 10:22:13 -0400

    Update navbar.jsx

--- src/front/js/component/navbar.jsx
+++ src/front/js/component/navbar.jsx
@@ -1,5 +1,3 @@
-// src/front/js/component/navbar.jsx
-
 import React, { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
